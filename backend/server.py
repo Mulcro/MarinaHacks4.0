e1b7778901e0e1f2de730930c16460f12bc590a6
Mulero Alamou <Mulero.alamou@gmail.com>
2024-10-26 16:32:54 -0700

    modified requests to send json response

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, request, session, jsonify
 from flask_socketio import SocketIO, emit, join_room
 import platform
 
@@ -11,23 +11,30 @@ users_in_room = {}
 rooms_sid = {}
 names_sid = {}
 
-
 @app.route("/join", methods=["GET"])
 def join():
     display_name = request.args.get('display_name')
-    mute_audio = request.args.get('mute_audio') # 1 or 0
-    mute_video = request.args.get('mute_video') # 1 or 0
+    mute_audio = request.args.get('mute_audio')  # 1 or 0
+    mute_video = request.args.get('mute_video')  # 1 or 0
     room_id = request.args.get('room_id')
-    session[room_id] = {"name": display_name,
-                        "mute_audio": mute_audio, "mute_video": mute_video}
-    return render_template("join.html", room_id=room_id, display_name=session[room_id]["name"], mute_audio=session[room_id]["mute_audio"], mute_video=session[room_id]["mute_video"])
-
+    session[room_id] = {
+        "name": display_name,
+        "mute_audio": mute_audio,
+        "mute_video": mute_video
+    }
+    
+    # Return JSON data instead of rendering a template
+    return jsonify({
+        "room_id": room_id,
+        "display_name": session[room_id]["name"],
+        "mute_audio": session[room_id]["mute_audio"],
+        "mute_video": session[room_id]["mute_video"]
+    })
 
 @socketio.on("connect")
 def on_connect():
     sid = request.sid
-    print("New socket connected ", sid)
-
+    print("New socket connected", sid)
 
 @socketio.on("join-room")
 def on_join_room(data):
@@ -35,30 +42,28 @@ def on_join_room(data):
     room_id = data["room_id"]
     display_name = session[room_id]["name"]
 
-    # register sid to the room
+    # Register sid to the room
     join_room(room_id)
     rooms_sid[sid] = room_id
     names_sid[sid] = display_name
 
-    # broadcast to others in the room
+    # Broadcast to others in the room
     print("[{}] New member joined: {}<{}>".format(room_id, display_name, sid))
     emit("user-connect", {"sid": sid, "name": display_name},
          broadcast=True, include_self=False, room=room_id)
 
-    # add to user list maintained on server
+    # Add to user list maintained on server
     if room_id not in users_in_room:
         users_in_room[room_id] = [sid]
-        emit("user-list", {"my_id": sid})  # send own id only
+        emit("user-list", {"my_id": sid})  # Send own id only
     else:
-        usrlist = {u_id: names_sid[u_id]
-                   for u_id in users_in_room[room_id]}
-        # send list of existing users to the new member
+        usrlist = {u_id: names_sid[u_id] for u_id in users_in_room[room_id]}
+        # Send list of existing users to the new member
         emit("user-list", {"list": usrlist, "my_id": sid})
-        # add new member to user list maintained on server
+        # Add new member to user list maintained on server
         users_in_room[room_id].append(sid)
 
-    print("\nusers: ", users_in_room, "\n")
-
+    print("\nusers:", users_in_room, "\n")
 
 @socketio.on("disconnect")
 def on_disconnect():
@@ -77,8 +82,7 @@ def on_disconnect():
     rooms_sid.pop(sid)
     names_sid.pop(sid)
 
-    print("\nusers: ", users_in_room, "\n")
-
+    print("\nusers:", users_in_room, "\n")
 
 @socketio.on("data")
 def on_data(data):
@@ -91,7 +95,6 @@ def on_data(data):
         print('{} message from {} to {}'.format(
             data["type"], sender_sid, target_sid))
     socketio.emit('data', data, room=target_sid)
-
 
 if any(platform.win32_ver()):
     socketio.run(app, debug=True)
