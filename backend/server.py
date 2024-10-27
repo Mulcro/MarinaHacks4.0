0655b9668896bf971e5b6bd1ba3545c185687b2e
Mulero Alamou <Mulero.alamou@gmail.com>
2024-10-26 21:19:25 -0700

    Created video streaming capabilities

--- backend/server.py
+++ backend/server.py
@@ -1,29 +1,33 @@
 from flask import Flask, request, session, jsonify
 from flask_socketio import SocketIO, emit, join_room
 import platform
+from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "wubba lubba dub dub"
+CORS(app)
 
-socketio = SocketIO(app)
+app.config['SECRET_KEY'] = "wubba lubba dub dub"
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 users_in_room = {}
 rooms_sid = {}
 names_sid = {}
 
-@app.route("/join", methods=["GET"])
+@app.route("/join", methods=["POST"])
 def join():
-    display_name = request.args.get('display_name')
-    mute_audio = request.args.get('mute_audio')  # 1 or 0
-    mute_video = request.args.get('mute_video')  # 1 or 0
-    room_id = request.args.get('room_id')
+    data = request.get_json()
+    
+    display_name = data.get('display_name')
+    mute_audio = data.get('audio')  # 1 or 0
+    mute_video = data.get('video')  # 1 or 0
+    room_id = 1
+    
     session[room_id] = {
         "name": display_name,
         "mute_audio": mute_audio,
         "mute_video": mute_video
     }
     
-    # Return JSON data instead of rendering a template
     return jsonify({
         "room_id": room_id,
         "display_name": session[room_id]["name"],
@@ -40,28 +44,36 @@ def on_connect():
 def on_join_room(data):
     sid = request.sid
     room_id = data["room_id"]
+
+    if room_id not in session:
+        print(f"Error: room_id {room_id} not found in session")
+        emit("error", {"message": "Session data for room not found"})
+        return
+
     display_name = session[room_id]["name"]
 
-    # Register sid to the room
     join_room(room_id)
     rooms_sid[sid] = room_id
     names_sid[sid] = display_name
 
-    # Broadcast to others in the room
     print("[{}] New member joined: {}<{}>".format(room_id, display_name, sid))
-    emit("user-connect", {"sid": sid, "name": display_name},
-         broadcast=True, include_self=False, room=room_id)
 
-    # Add to user list maintained on server
+    # Emit the new user connection to other members in the room
+    emit("user-connect", {"sid": sid, "name": display_name}, broadcast=True, include_self=False, room=room_id)
+
+    # Notify existing users of the new user and send them existing stream offers
     if room_id not in users_in_room:
         users_in_room[room_id] = [sid]
         emit("user-list", {"my_id": sid})  # Send own id only
     else:
         usrlist = {u_id: names_sid[u_id] for u_id in users_in_room[room_id]}
-        # Send list of existing users to the new member
         emit("user-list", {"list": usrlist, "my_id": sid})
-        # Add new member to user list maintained on server
         users_in_room[room_id].append(sid)
+
+        # Notify the new user of existing streams and send offers
+        for existing_sid in users_in_room[room_id]:
+            if existing_sid != sid:  # Don't send to self
+                emit("user-connect", {"sid": existing_sid, "name": names_sid[existing_sid]}, room=sid)
 
     print("\nusers:", users_in_room, "\n")
 
@@ -72,8 +84,7 @@ def on_disconnect():
     display_name = names_sid[sid]
 
     print("[{}] Member left: {}<{}>".format(room_id, display_name, sid))
-    emit("user-disconnect", {"sid": sid},
-         broadcast=True, include_self=False, room=room_id)
+    emit("user-disconnect", {"sid": sid}, broadcast=True, include_self=False, room=room_id)
 
     users_in_room[room_id].remove(sid)
     if len(users_in_room[room_id]) == 0:
@@ -92,8 +103,7 @@ def on_data(data):
         print("[Not supposed to happen!] request.sid and sender_id don't match!!!")
 
     if data["type"] != "new-ice-candidate":
-        print('{} message from {} to {}'.format(
-            data["type"], sender_sid, target_sid))
+        print('{} message from {} to {}'.format(data["type"], sender_sid, target_sid))
     socketio.emit('data', data, room=target_sid)
 
 if any(platform.win32_ver()):
