fa88113536b4b6b19f0840b95ebe6af6cf8a85b3
Mulero Alamou <Mulero.alamou@gmail.com>
2024-10-27 09:48:15 -0700

    added procfile

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, session, jsonify
+from flask import Flask, request, jsonify
 from flask_socketio import SocketIO, emit, join_room
 import platform
 from flask_cors import CORS
@@ -9,6 +9,7 @@ CORS(app)
 app.config['SECRET_KEY'] = "wubba lubba dub dub"
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+session = {}
 users_in_room = {}
 rooms_sid = {}
 names_sid = {}
@@ -18,8 +19,8 @@ def join():
     data = request.get_json()
     
     display_name = data.get('display_name')
-    mute_audio = data.get('audio')  # 1 or 0
-    mute_video = data.get('video')  # 1 or 0
+    mute_audio = data.get('audio') 
+    mute_video = data.get('video')  
     room_id = 1
     
     session[room_id] = {
@@ -39,11 +40,14 @@ def join():
 def on_connect():
     sid = request.sid
     print("New socket connected", sid)
+    print("Session:", session)
 
 @socketio.on("join-room")
 def on_join_room(data):
     sid = request.sid
     room_id = data["room_id"]
+    print('SID:', sid)
+    print('room_id:', room_id)
 
     if room_id not in session:
         print(f"Error: room_id {room_id} not found in session")
@@ -61,37 +65,43 @@ def on_join_room(data):
     # Emit the new user connection to other members in the room
     emit("user-connect", {"sid": sid, "name": display_name}, broadcast=True, include_self=False, room=room_id)
 
-    # Notify existing users of the new user and send them existing stream offers
     if room_id not in users_in_room:
         users_in_room[room_id] = [sid]
         emit("user-list", {"my_id": sid})  # Send own id only
     else:
-        usrlist = {u_id: names_sid[u_id] for u_id in users_in_room[room_id]}
+        usrlist = {u_id: names_sid.get(u_id, "Unknown") for u_id in users_in_room[room_id]}
+
         emit("user-list", {"list": usrlist, "my_id": sid})
         users_in_room[room_id].append(sid)
 
         # Notify the new user of existing streams and send offers
         for existing_sid in users_in_room[room_id]:
             if existing_sid != sid:  # Don't send to self
-                emit("user-connect", {"sid": existing_sid, "name": names_sid[existing_sid]}, room=sid)
+                existing_name = names_sid.get(existing_sid, "Unknown User")
+                emit("user-connect", {"sid": existing_sid, "name": existing_name}, room=sid)
 
     print("\nusers:", users_in_room, "\n")
 
 @socketio.on("disconnect")
 def on_disconnect():
     sid = request.sid
-    room_id = rooms_sid[sid]
-    display_name = names_sid[sid]
+    room_id = rooms_sid.get(sid)
+    if room_id is None:
+        print(f"Error: SID {sid} is not associated with any room.")
+        return
+
+    display_name = names_sid.get(sid, "Unknown User")
 
     print("[{}] Member left: {}<{}>".format(room_id, display_name, sid))
     emit("user-disconnect", {"sid": sid}, broadcast=True, include_self=False, room=room_id)
 
-    users_in_room[room_id].remove(sid)
-    if len(users_in_room[room_id]) == 0:
-        users_in_room.pop(room_id)
+    if room_id in users_in_room and sid in users_in_room[room_id]:
+        users_in_room[room_id].remove(sid)
+        if not users_in_room[room_id]:  # Remove room if empty
+            users_in_room.pop(room_id)
 
-    rooms_sid.pop(sid)
-    names_sid.pop(sid)
+    rooms_sid.pop(sid, None)
+    names_sid.pop(sid, None)
 
     print("\nusers:", users_in_room, "\n")
 
