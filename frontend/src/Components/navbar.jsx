4eff47489724cf34c2caa54867d617bd3815737e
Mulero Alamou <Mulero.alamou@gmail.com>
2024-10-27 09:25:54 -0700

    changed height unit from rem to vh

--- frontend/src/Components/navbar.jsx
+++ frontend/src/Components/navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
     const arr = [1,1,1,1,1,1,1,1,1,1 ]
     return (
-    <div className="bg-[#F2CFF2] h-[3rem] flex flex-row gap-9 items-center font-silkscreen text-sm">
+    <div className="bg-[#F2CFF2] h-[8vh] flex flex-row gap-9 items-center font-silkscreen text-sm">
         {arr.map((elem,indx) => {
             return (
                 <h4>Chat & Learn</h4>
